2c08ecc0f9daf397f13f20c7d09ce535b874a1c0
felice <fli50@stuy.edu>
2024-05-17 09:07:52 -0400

    new print statement

--- test.java
+++ test.java
@@ -1,1 +1,7 @@
-System.out.println("heehoo");
+class test {
+    public void main(String[] args) {
+        System.out.println("heehoo");
+
+        System.out.println("i am felice and i am cool");
+    }
+}
